6b192b2ab00a4b59f30ddef53495880f71cfad15
Pedro Holguín <holguin98fx@gmail.com>
2026-03-19 09:57:57 -0500

    Configs github pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Providers } from "./providers";
 export const metadata: Metadata = {
   title: "Pedro Holguín",
   description:
-    "Soluciones web, móviles e integraciones con IA para impulsar tu negocio",
+    "Soluciones web, móviles e integraciones con IA para impulsar tu negocio.",
   icons: {
     icon: "/images/icon.svg",
   },
